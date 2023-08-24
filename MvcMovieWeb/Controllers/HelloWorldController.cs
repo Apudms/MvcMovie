67b6ed1ab34939b7c90570028e209081c64f9736
Muhammad Saefudin <itsapud@gmail.com>
2023-08-24 21:22:42 +0700

    Add new controller

---
+++ MvcMovieWeb/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovieWeb.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
