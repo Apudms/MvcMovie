8857d22e647dcda4f45fb5254daaef636ec04369
Muhammad Saefudin <itsapud@gmail.com>
2023-08-30 09:48:19 +0700

    Add a view

--- MvcMovieWeb/Controllers/HelloWorldController.cs
+++ MvcMovieWeb/Controllers/HelloWorldController.cs
@@ -5,16 +5,16 @@ namespace MvcMovieWeb.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         // GET: /HelloWorld/
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
 
         // GET: /HelloWorld/Welcome/
         // Requires using System.Text.Encodings.Web;
@@ -23,9 +23,17 @@ namespace MvcMovieWeb.Controllers
         //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         //}
 
-        public string Welcome(string name, int ID = 1)
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Halo " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
